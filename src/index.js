ad3c98a10a2fb155a75d98197f473cd7202b9e2b
manoj <harsh.pandey@crematrix.com>
2025-08-04 15:10:55 +0530

    fix: v1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// 1. Import the service worker registration file
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +17,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// 2. Change unregister() to register() to enable the service worker
+// This will allow your app to work offline and load faster.
+// Learn more about service workers and PWAs: https://cra.link/PWA
+serviceWorkerRegistration.register();
